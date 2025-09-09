6aabe51137f96c65d77a3cd76a2938fdb3bb2d2c
Pengkun Zhang <grayrave@163.com>
2025-09-09 21:30:20 +0800

    clear

--- scripts/tabicl_evaluate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python ./src/evaluation/tabicl_evaluate.py \
-    --datasets heloc \
-    --data_dir ./datahub_inputs/data_raw \
-    --output_dir ./datahub_outputs/tabicl \
-    --n_jobs 4 \
-    --sample_sizes 8 16 32 64 128 256 512 1024
